25f536cebfc3003f1192e2b99b084e1e03f93c7b
Jun Bin <autumnlewjb@gmail.com>
2020-07-01 17:06:25 +0800

    create new file for backend

---
+++ GUI/src/backend/MainBackend.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+import backend.Hangman;
+import java.awt.AWTException;
+import java.io.IOException;
+
+/**
+ *
+ * @author Autumn
+ */
+public class MainBackend {
+    public MainBackend() {
+        init();
+    }
+    
+    public void init() {
+        System.out.println("let's begin");
+    }
+}

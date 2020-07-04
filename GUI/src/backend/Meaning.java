ed123b21a95a2323cba665be3e8f13a2923fcd84
Jun Bin <autumnlewjb@gmail.com>
2020-07-05 00:05:26 +0800

    successful api connection

--- GUI/src/backend/Meaning.java
+++ GUI/src/backend/Meaning.java
@@ -5,10 +5,63 @@
  */
 package backend;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import javax.net.ssl.HttpsURLConnection;
+
 /**
  *
  * @author Autumn
  */
 public class Meaning {
     
+    public String app_id;
+    public String app_key;
+    public URL url;
+    public String word;
+    
+    public Meaning(String app_key, String word) {
+        this.app_key = app_key;
+        this.word = word;
+        this.url = this.setURL();
+    }
+    
+    public static void main(String[] args) {
+        Meaning meaning = new Meaning("", "hello"); // insert your api key in the first positional argument
+        try {
+            System.out.println(meaning.sendGet());
+        }catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        
+    }
+    
+    private URL setURL() {
+        String tmp = "https://dictionaryapi.com/api/v3/references/learners/json/" + this.word + "?key=" + this.app_key;
+        try {
+            return new URL(tmp);
+        }catch(MalformedURLException e) {
+            return null;
+        }
+    }
+    
+    public String sendGet() throws IOException{
+        HttpsURLConnection conn = (HttpsURLConnection)this.url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        if (conn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+            Scanner sc = new Scanner(this.url.openStream());
+            StringBuilder result = new StringBuilder();
+            while (sc.hasNextLine()) {
+                result.append(sc.nextLine());
+            }
+            sc.close();
+            conn.disconnect();
+            return result.toString();
+        }else {
+            return Integer.toString(conn.getResponseCode());
+        }
+    }
 }
